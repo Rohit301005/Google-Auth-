5d807e9e96045fabcbd6214f703b13988cd48891
Rohit301005 <rohitkumarrrawat04@gmail.com>
2026-09-07 14:03:13 +0530

    ..

--- AuthenticationProject/settings.py
+++ AuthenticationProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-_sb+#bi*!9dtu2!h#uxca@%iqxxoju1@w0f2h=50*m&ef@pxgo"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
@@ -112,6 +113,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'Core.User'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
@@ -126,4 +129,11 @@ MAILERS = {
     "default": {
         "BACKEND": "django.core.mail.backends.console.EmailBackend",
     },
+}
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
--- Core/views.py
+++ Core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+# def home(request):
+#     return HttpRequest("Home Page")
+
+@api_view(["GET"])
+def home(request):
+    return Response("Hello")
